af59276f8c3c7545cac774d783fa292b0a952287
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-03-27 19:31:21 +0500

    Footer And Purchase Pages Fixed

--- Js/MachineLearning-3.js
+++ Js/MachineLearning-3.js
@@ -2,18 +2,19 @@ window.addEventListener('scroll', () =>  {
     let purchase = document.getElementById('purchase-section')
     let footer = document.getElementById('footer')
     let purchaseHeight = purchase.offsetLeftsetHeight;
-    let footerPosition = footer.getBoundingClientRect().top;
+    let footerPosition = footer.getBoundingClientRect().top
 
      // Check if footer is in view
  if (footerPosition <= window.innerHeight) {
-   purchase.classList.add('hidden')
+   purchase.classList.add('hidden');   
 } 
 
 else {
-   purchase.classList.remove('hidden')
+   purchase.classList.remove('hidden'); 
    
 }
 });
 
 console.log('footerPosition')
+
 
